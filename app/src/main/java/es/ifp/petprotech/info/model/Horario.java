0e40ed81e1e84ce186165a509e8400082ab7298d
Enrique Verea <kikeverea@gmail.com>
2023-12-19 03:50:18 +0100

    Refactoriza clase Horario

--- app/src/main/java/es/ifp/petprotech/info/model/Horario.java
+++ app/src/main/java/es/ifp/petprotech/info/model/Horario.java
@@ -1,72 +1,90 @@
 package es.ifp.petprotech.info.model;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Horario {
 
-    private final HorarioDia lunes;
-    private final HorarioDia martes;
-    private final HorarioDia miercoles;
-    private final HorarioDia jueves;
-    private final HorarioDia viernes;
-    private final HorarioDia sabado;
-    private final HorarioDia domingo;
+    private final HorarioDia[] horario;
+    private final HorarioDia festivos;
 
     public Horario(Builder builder) {
-        miercoles = builder.miercoles;
-        sabado = builder.sabado;
-        martes = builder.martes;
-        lunes = builder.lunes;
-        domingo = builder.domingo;
-        viernes = builder.viernes;
-        jueves = builder.jueves;
+        horario = builder.horario;
+        festivos = builder.festivos;
+    }
+
+    public HorarioDia[] getHorario() {
+        return horario;
+    }
+    public HorarioDia getFestivos() {
+        return festivos;
     }
 
     public static Builder nuevo() {
         return new Builder();
     }
 
+    /**
+     * Crea un nuevo horario con el mismo horario para cada dia de la semana, excepto los dias de
+     * descanso
+     * @param horarioFijo El horario que será asignado a cada dia de la semana, excepto los de
+     *                    descanso
+     * @param diasDescanso Los días de la semana a los que no se le asignará un horario
+     * @return Una nueva instancia de Builder
+     */
+    public static Builder nuevo(HorarioDia horarioFijo, int... diasDescanso) {
+        return new Builder();
+    }
+
     public static final class Builder {
-        private HorarioDia lunes;
-        private HorarioDia martes;
-        private HorarioDia miercoles;
-        private HorarioDia jueves;
-        private HorarioDia viernes;
-        private HorarioDia sabado;
-        private HorarioDia domingo;
+        private HorarioDia[] horario = new HorarioDia[7];
+        private HorarioDia festivos;
 
         private Builder() {}
 
+        private Builder(HorarioDia horarioFijo, int... diasDescanso) {
+            horario = IntStream.range(0, 8)
+                .mapToObj(ind -> Arrays.stream(diasDescanso).anyMatch(descanso -> descanso == ind) ? null : horarioFijo)
+                .toArray(HorarioDia[]::new);
+        }
+
         public Builder lunes(HorarioDia lunes) {
-            this.lunes = lunes;
+            horario[0] = lunes;
             return this;
         }
 
         public Builder martes(HorarioDia martes) {
-            this.martes = martes;
+            horario[1] = martes;
             return this;
         }
 
         public Builder miercoles(HorarioDia miercoles) {
-            this.miercoles = miercoles;
+            horario[2] = miercoles;
             return this;
         }
 
         public Builder jueves(HorarioDia jueves) {
-            this.jueves = jueves;
+            horario[3] = jueves;
             return this;
         }
 
         public Builder viernes(HorarioDia viernes) {
-            this.viernes = viernes;
+            horario[4] = viernes;
             return this;
         }
 
         public Builder sabado(HorarioDia sabado) {
-            this.sabado = sabado;
+            horario[5] = sabado;
             return this;
         }
 
         public Builder domingo(HorarioDia domingo) {
-            this.domingo = domingo;
+            horario[6] = domingo;
+            return this;
+        }
+
+        public Builder festivos(HorarioDia festivos) {
+            this.festivos = festivos;
             return this;
         }
 
